191d74639b5aebda73fd15eabc3bdb47a45087b2
Yasmin Kioko <yasmin_kioko@hotmail.com>
2024-09-16 01:43:00 -0300

    teste

--- calculadora.py
+++ calculadora.py
@@ -8,26 +8,8 @@ class operations:
     def subtraction(self, x,y):
         return x-y
 
-    def muntiplication(self, x,y):
+    def multiplication(self, x,y):
         return x*y
 
     def division(self, x,y):
         return x/y if y != 0 else None
-    
-    def pow(x,y):
-        return math.pow(x,y)
-
-    def sqrt(x):
-        return math.sqrt(x)
-    
-    def acos(x):
-        return math.acos(x)
-    
-    def asen(x):
-        return math.asin(x)
-    
-    def atan(x):
-        return math.atan(x)
-    
-    def atan2(x,y):
-        return math.atan2(x,y)
