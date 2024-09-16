09abf0e9266b962f9d9e58d73fc003f1e5d24b9b
Yasmin Kioko <yasmin_kioko@hotmail.com>
2024-09-16 16:40:22 -0300

    update

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD [ "python", "./teste.py" ]
+CMD [ "python", "./calculadora.py" ]
--- calculadora.py
+++ calculadora.py
@@ -1,15 +1,36 @@
-#import math
+import sys
 
-class operations:
+def sum(self, x,y):
+    return x+y
 
-    def sum(self, x,y):
-        return x+y
+def subtraction(self, x,y):
+    return x-y
 
-    def subtraction(self, x,y):
-        return x-y
+def multiplication(self, x,y):
+    return x*y
 
-    def multiplication(self, x,y):
-        return x*y
+def division(self, x,y):
+    return x/y if y != 0 else None
 
-    def division(self, x,y):
-        return x/y if y != 0 else None
+def main():
+    if len(sys.argv) != 4:
+        print("python3 calculadora.py <operação> <número1> <número2>")
+        return
+
+    operacao = sys.argv[1]
+    num1 = float(sys.argv[2])
+    num2 = float(sys.argv[3])
+
+    if operacao == '1':
+        print("Result:", sum(num1, num2))
+    elif operacao == '2':
+        print("Result:", subtraction(num1, num2))
+    elif operacao == '3':
+        print("Result:", multiplication(num1, num2))
+    elif operacao == '4':
+        print("Result:", division(num1, num2))
+    else:
+        print("Invalid")
+
+if __name__ == "__main__":
+    main()
